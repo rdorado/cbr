dc3ab4c60f67ab2835db161777331e1b7aad7630
rdorado <ruben.dorados@gmail.com>
2016-04-21 17:16:08 -0400

    first commit

--- src/cbr/test/sum/SumTest.java
+++ src/cbr/test/sum/SumTest.java
@@ -6,7 +6,7 @@ public class SumTest {
 
 	public static void main(String[] args) {
 		CBR<SumProblem, SumSolution> cbr = new CBR<SumProblem, SumSolution>( SumProblem.class, new SumAlgorithm() );
-		
+		 
 		/*
 		 * Some cases for the CBR
 		 */
@@ -14,27 +14,26 @@ public class SumTest {
 		SumProblem case1 = new SumProblem();
 		case1.a = 1;
 		case1.b = 4;		
-		cbr.solveCase(case1);
+		SumSolution r1 = cbr.solveCase(case1);
 
 		/** Case 2*/
 		SumProblem case2 = new SumProblem();
 		case2.a = 100;
 		case2.b = -90;		
-		cbr.solveCase(case2);
+		SumSolution r2 = cbr.solveCase(case2);
 		
 		/** Case 3*/
 		SumProblem case3 = new SumProblem();
 		case3.a = -10;
 		case3.b = 4;		
-		cbr.solveCase(case3);
+		SumSolution r3 = cbr.solveCase(case3);
 
 		/** Case 4*/
 		SumProblem case4 = new SumProblem();
 		case4.a = 0;
 		case4.b = 0;		
-		cbr.solveCase(case4);		
+		SumSolution r4 = cbr.solveCase(case4);		
 		
-		System.out.println(SumAlgorithm.class.getName());
 		
 		/** Displays the stored knowledge*/
 		cbr.showKnowledge();
